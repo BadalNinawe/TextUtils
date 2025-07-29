fefdd8756cfb46b7ee12f2824793683a6be76c5c
BadalNinawe <badalninawe@gmail.com>
2025-07-29 10:04:01 +0530

    add files

---
+++ src/Component/About.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+export default function About(props) {
+  const myStyle = {
+    color: props.mode === "dark" ? "white" : "#042743",
+    backgroundColor: props.mode === "dark" ? "rgb(36 74 104)" : "white",
+    border: "1px solid",
+    borderColor: props.mode === "dark" ? "#ffffff40" : "#00000020",
+  };
+
+  return (
+    <div className="container">
+      <h1
+        className="my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        About Us
+      </h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              <strong>Analyze Your Text</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils allows you to analyze your text quickly and efficiently
+              — whether it's word count, character count, or removing extra
+              spaces.
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <strong>Free to Use</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a free tool that provides instant word and character
+              statistics. It's perfect for writing with length limits in mind —
+              like social media, essays, or form inputs.
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              <strong>Browser Compatible</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              This tool works in all major web browsers including Chrome,
+              Firefox, Safari, Opera, and Edge. It’s suitable for use in
+              documents like blogs, PDFs, Word files, or online text editors.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
